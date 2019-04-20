f28e5e35a9bbc53a9938c922bde3a02a8adc01c5
Joseph Martinsen <joseph@martinsen.com>
2019-04-20 00:53:22 -0500

    now returns 100 results

--- src/solr.py
+++ src/solr.py
@@ -8,11 +8,12 @@ CORE_NAME = os.getenv("CORE_NAME")
 
 SERVER = f"{URL}/solr/{CORE_NAME}/select"
 
-def autocomplete(query: str, over18: bool=False) -> List[dict]:
+
+def autocomplete(query: str, over18: bool = False) -> List[dict]:
     params = {
         "q": f"display_name:*{query}* AND over18:{str(over18).lower()}",
         "sort": "subscribers desc",
-        "rows": 10
+        "rows": 10,
     }
 
     r = requests.get(SERVER, params)
@@ -20,11 +21,12 @@ def autocomplete(query: str, over18: bool=False) -> List[dict]:
 
     return [doc["display_name"] for doc in body["response"]["docs"]]
 
+
 # returns a list of subreddits from the query that don't exist
 def validate_query(subreddits: List[str]) -> List[Subreddit]:
     params = {
-        "q": " OR ".join(f'display_name:{sub}' for sub in subreddits),
-        "rows": len(subreddits)
+        "q": " OR ".join(f"display_name:{sub}" for sub in subreddits),
+        "rows": len(subreddits),
     }
 
     r = requests.get(SERVER, params)
@@ -36,7 +38,7 @@ def validate_query(subreddits: List[str]) -> List[Subreddit]:
         for doc in body["response"]["docs"]:
             s.append(doc["display_name"])
 
-        not_found =  set(subreddits).symmetric_difference(set(s))
+        not_found = set(subreddits).symmetric_difference(set(s))
         raise Exception(f"Subreddit(s) not found: {', '.join(f for f in not_found)}")
 
     subs: List[Subreddit] = []
@@ -45,12 +47,16 @@ def validate_query(subreddits: List[str]) -> List[Subreddit]:
 
     return subs
 
+
 # returns (display_names, public description)
 def get_docs(words: Set[str]) -> List[Subreddit]:
     params = {
-        "q": " OR ".join(f'public_description:"{word}" OR title:*{word}* OR display_name:*{word}*' for word in words),
-        "rows": 15,
-        "sort": "subscribers desc"
+        "q": " OR ".join(
+            f'public_description:"{word}" OR title:*{word}* OR display_name:*{word}*'
+            for word in words
+        ),
+        "rows": 100,
+        "sort": "subscribers desc",
     }
 
     r = requests.get(SERVER, params)
